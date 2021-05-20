d0f47fdbcc5d84ed43930e7d58c29c9445c74389
Titouan <parcollet.titouan@gmail.com>
2021-05-20 11:46:19 +0200

    print

--- recipes/downstream_librispeech/ASR/seq2seq/train/train_finetune.py
+++ recipes/downstream_librispeech/ASR/seq2seq/train/train_finetune.py
@@ -83,6 +83,7 @@ class ASR(sb.Brain):
         else:
             embeddings = PASE_brain.modules.enc(feats)
 
+        print(PASE_brain.modules.enc)
         x = self.modules.enc(embeddings)
 
         e_in = self.modules.emb(tokens_bos)  # y_in bos + tokens
