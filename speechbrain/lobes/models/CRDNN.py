29bb8a17b2df6e663a75b4a8e5c05ca00c0f6dd6
Titouan <parcollet.titouan@gmail.com>
2021-06-10 16:27:28 +0200

    gneih

--- speechbrain/lobes/models/CRDNN.py
+++ speechbrain/lobes/models/CRDNN.py
@@ -123,16 +123,16 @@ class CRDNN(sb.nnet.containers.Sequential):
                 layer_name=f"block_{block_index}",
             )
 
-        # if time_pooling:
-        #    self.append(
-        #        sb.nnet.pooling.Pooling1d(
-        #            pool_type="max",
-        #            input_dims=4,
-        #            kernel_size=time_pooling_size,
-        #            pool_axis=1,
-        #        ),
-        #        layer_name="time_pooling",
-        #    )
+        if time_pooling:
+            self.append(
+                sb.nnet.pooling.Pooling1d(
+                    pool_type="max",
+                    input_dims=4,
+                    kernel_size=time_pooling_size,
+                    pool_axis=1,
+                ),
+                layer_name="time_pooling",
+            )
 
         # This projection helps reducing the number of parameters
         # when using large number of CNN filters.
@@ -237,25 +237,26 @@ class CNN_Block(sb.nnet.containers.Sequential):
         self.append(
             sb.nnet.CNN.Conv2d,
             out_channels=channels,
-            kernel_size=(3, 3),
+            kernel_size=kernel_size,
             layer_name="conv_1",
         )
-        # self.append(sb.nnet.normalization.LayerNorm, layer_name="norm_1")
+        self.append(sb.nnet.normalization.LayerNorm, layer_name="norm_1")
         self.append(activation(), layer_name="act_1")
         self.append(
             sb.nnet.CNN.Conv2d,
             out_channels=channels,
-            kernel_size=(3, 3),
+            kernel_size=kernel_size,
             layer_name="conv_2",
         )
-        # self.append(sb.nnet.normalization.LayerNorm, layer_name="norm_2")
+        self.append(sb.nnet.normalization.LayerNorm, layer_name="norm_2")
         self.append(activation(), layer_name="act_2")
 
         if using_2d_pool:
-            print("hello")
             self.append(
                 sb.nnet.pooling.Pooling2d(
-                    pool_type="max", kernel_size=(2, 2), pool_axis=(1, 2),
+                    pool_type="max",
+                    kernel_size=(pooling_size, pooling_size),
+                    pool_axis=(1, 2),
                 ),
                 layer_name="pooling",
             )
