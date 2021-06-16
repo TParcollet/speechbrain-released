34908a158335d3232c923712b1716002a03bf45f
Titouan <parcollet.titouan@gmail.com>
2021-06-16 18:26:53 +0200

    multiple

--- recipes/mustc-v1/ST/seq2seq/train.py
+++ recipes/mustc-v1/ST/seq2seq/train.py
@@ -95,16 +95,22 @@ class ASR(sb.core.Brain):
             stage == sb.Stage.TRAIN
             and current_epoch < self.hparams.number_of_ctc_epochs
         ):
-            print("hello")
             loss_ctc = self.hparams.ctc_cost(
                 p_ctc, tokens, wav_lens, tokens_lens
             )
             loss = self.hparams.ctc_weight * loss_ctc
             loss += (1 - self.hparams.ctc_weight) * loss_seq
         else:
-            print("hello2")
+            old_correct = self.acc_train_metric.correct
+            old_total = self.acc_train_metric.total
             self.acc_train_metric.append(p_seq, tokens_eos, tokens_eos_lens)
             print(self.acc_train_metric.summarize())
+            self.acc_train_metric.correct = 0
+            self.acc_train_metric.total = 0
+            self.acc_train_metric.append(p_seq, tokens_eos, tokens_eos_lens)
+            print(self.acc_train_metric.summarize())
+            self.acc_train_metric.correct = old_correct
+            self.acc_train_metric.total = old_total
             loss = loss_seq
 
         if stage != sb.Stage.TRAIN:
