6764d5aec842048f0948d4a02d27fe46533e4045
Titouan <parcollet.titouan@gmail.com>
2021-06-20 16:27:20 +0200

    0

--- recipes/mustc-v1/ST/seq2seq/train.py
+++ recipes/mustc-v1/ST/seq2seq/train.py
@@ -101,8 +101,8 @@ class ASR(sb.core.Brain):
             loss = self.hparams.ctc_weight * loss_ctc
             loss += (1 - self.hparams.ctc_weight) * loss_seq
         else:
-            print(p_seq)
-            print(tokens_eos)
+            print(p_seq[0])
+            print(tokens_eos[0])
             print("---")
             self.acc_train_metric.append(p_seq, tokens_eos, tokens_eos_lens)
             logger.info(self.acc_train_metric.summarize())
