a5505c0129712a3adebb0cadc56949a071194804
Titouan <parcollet.titouan@gmail.com>
2021-06-23 16:00:11 +0200

    decode

--- recipes/mustc-v1/ST/seq2seq/train_infer.py
+++ recipes/mustc-v1/ST/seq2seq/train_infer.py
@@ -116,7 +116,7 @@ class ASR(sb.core.Brain):
             # predicted_words = self.tokenizer(
             #    predicted_tokens, task="decode_from_list"
             # )
-            print(p_seq.argmax(-1)[0])
+            print(self.tokenizer.sp.decode_ids(p_seq.argmax(-1)[0]))
             # Convert indices to words
             # target_words = undo_padding(tokens_eos, tokens_eos_lens)
             # target_words = self.tokenizer(target_words, task="decode_from_list")
