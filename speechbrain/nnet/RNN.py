3e7e7b2e00b12c06662cb629f78303d6419f7632
Titouan <parcollet.titouan@gmail.com>
2021-06-23 23:01:55 +0200

    yapluka

--- speechbrain/nnet/RNN.py
+++ speechbrain/nnet/RNN.py
@@ -950,7 +950,6 @@ class AttentionalRNNDecoder(nn.Module):
                 inp = inp_tensor[:, t]
             else:
                 if torch.rand(1) < 0.2:
-                    logger.info("sampling")
                     inp = outputs
                 else:
                     inp = inp_tensor[:, t]
