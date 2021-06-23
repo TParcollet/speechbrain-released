7ed92cc269063ebffb9d9e4eaf6c27b8be3309b2
Titouan <parcollet.titouan@gmail.com>
2021-06-23 12:04:15 +0200

    lol

--- speechbrain/utils/Accuracy.py
+++ speechbrain/utils/Accuracy.py
@@ -39,6 +39,7 @@ def Accuracy(log_probabilities, targets, length=None):
         numerator = torch.sum(
             padded_pred.masked_select(mask) == targets.masked_select(mask)
         )
+        print(padded_pred.masked_select(mask) == targets.masked_select(mask))
         print(padded_pred.masked_select(mask))
         print(targets.masked_select(mask))
         denominator = torch.sum(mask)
