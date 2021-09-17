324911c25bc30b934a47b5aa5add90a267481142
Titouan <parcollet.titouan@gmail.com>
2021-09-17 17:25:26 +0200

    test

--- recipes/LibriSpeech/ASR/ctc/train_with_wav2vec.py
+++ recipes/LibriSpeech/ASR/ctc/train_with_wav2vec.py
@@ -347,9 +347,9 @@ if __name__ == "__main__":
         print(
             asr_brain.modules.wav2vec2.model.feature_projection.layer_norm.weight
         )
-        asr_brain.modules.wav2vec2.model = (
-            asr_brain.modules.wav2vec2_trans.model.wav2vec2
-        )
+        asr_brain.modules.wav2vec2.model = hparams[
+            "pretrainer"
+        ].wav2vec2_trans.model.wav2vec2
         print(
             asr_brain.modules.wav2vec2.model.feature_projection.layer_norm.weight
         )
