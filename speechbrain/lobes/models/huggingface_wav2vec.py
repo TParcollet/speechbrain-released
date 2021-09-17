ca2b759d223fbb116fb7b35b15cf6ee25de049dd
Titouan <parcollet.titouan@gmail.com>
2021-09-17 15:50:16 +0200

    pretrain for HF

--- speechbrain/lobes/models/huggingface_wav2vec.py
+++ speechbrain/lobes/models/huggingface_wav2vec.py
@@ -12,10 +12,15 @@ Authors
 import torch
 import torch.nn.functional as F
 from torch import nn
+import speechbrain as sb
 
 # We check if transformers is installed.
 try:
-    from transformers import Wav2Vec2Model, Wav2Vec2Config
+    from transformers import (
+        Wav2Vec2Model,
+        Wav2Vec2Config,
+        Wav2Vec2ForPreTraining,
+    )
     from transformers import Wav2Vec2FeatureExtractor
 except ImportError:
     print("Please install transformer from HuggingFace to use wav2vec2!")
@@ -146,3 +151,94 @@ class HuggingFaceWav2Vec2(nn.Module):
             out = F.layer_norm(out, out.shape)
 
         return out
+
+
+class HuggingFaceWav2Vec2ForPretrain(nn.Module):
+    """This lobe enables the integration of HuggingFace
+    pretrained wav2vec2.0 models.
+
+    Source paper: https://arxiv.org/abs/2006.11477
+    Transformer from HuggingFace needs to be installed:
+    https://huggingface.co/transformers/installation.html
+
+    The model can be used as a fixed feature extractor or can be finetuned. It
+    will download automatically the model from HuggingFace.
+
+    Arguments
+    ---------
+    source : str
+        HuggingFace hub name: e.g "facebook/wav2vec2-large-lv60"
+    save_path : str
+        Path (dir) of the downloaded model.
+    output_norm : bool (default: True)
+        If True, a layer_norm (affine) will be applied to the output obtained
+        from the wav2vec model.
+    freeze : bool (default: True)
+        If True, the model is frozen. If False, the model will be trained
+        alongside with the rest of the pipeline.
+    freeze_feature_extractor :  bool (default: False)
+        When freeze = False and freeze_feature_extractor True, the featue_extractor module of the model is Frozen. If False
+        all the wav2vec model will be trained including featue_extractor module.
+    pretrain : bool (default: True)
+        If True, the model is pretrained with the specified source.
+        If False, the randomly-initialized model is instantiated.
+    apply_spec_augment : bool (default: False)
+        If True, the model will apply spec augment on the output of feature extractor
+        (inside huggingface Wav2VecModel() class).
+        If False, the model will not apply spec augment. We set this to false to prevent from doing it twice.
+    Example
+    -------
+    >>> inputs = torch.rand([10, 600])
+    >>> model_hub = "facebook/wav2vec2-base-960h"
+    >>> save_path = "savedir"
+    >>> model = HuggingFaceWav2Vec2(model_hub, save_path)
+    >>> outputs = model(inputs)
+    >>> outputs.shape
+    torch.Size([10, 1,  768])
+    """
+
+    def __init__(
+        self, source, save_path, pretrain=True,
+    ):
+        super().__init__()
+
+        # Download the extractor from HuggingFace.
+        # The extractor is only used to retrieve the normalisation
+        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
+            source, cache_dir=save_path
+        )
+
+        # Download the model from HuggingFace.
+        # if pretrain is False, we do not download the pretrained weights
+        # it it is True, we download and load them.
+        if not (pretrain):
+            config = Wav2Vec2Config.from_pretrained(source, cache_dir=save_path)
+            config.gradient_checkpointing = False
+            self.model = Wav2Vec2ForPreTraining(config)
+        else:
+            self.model = Wav2Vec2ForPreTraining.from_pretrained(
+                source, cache_dir=save_path
+            )
+
+        # We check if inputs need to be normalized w.r.t pretrained wav2vec2
+        self.normalize_wav = self.feature_extractor.do_normalize
+
+    def forward(self, wav, mask_time_indices, stage):
+        """Takes an input waveform and return its corresponding wav2vec encoding.
+
+        Arguments
+        ---------
+        wav : torch.Tensor (signal)
+            A batch of audio signals to transform to features.
+        """
+
+        if self.normalize_wav:
+            wav = F.layer_norm(wav, wav.shape)
+
+        if stage == sb.Stage.TRAIN:
+            out = self.model(wav, mask_time_indices=mask_time_indices)
+            loss = out.loss
+            return loss
+        else:
+            out = self.model(wav, mask_time_indices=mask_time_indices)
+            return out
