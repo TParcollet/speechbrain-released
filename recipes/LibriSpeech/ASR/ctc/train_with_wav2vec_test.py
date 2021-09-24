d69f5f4adca80282428abc5e3bfd8158569ee221
Titouan <parcollet.titouan@gmail.com>
2021-09-24 16:13:37 +0200

    encoder

--- recipes/LibriSpeech/ASR/ctc/train_with_wav2vec_test.py
+++ recipes/LibriSpeech/ASR/ctc/train_with_wav2vec_test.py
@@ -63,7 +63,7 @@ class ASR(sb.Brain):
         feats = self.modules.wav2vec2_fairseq(wavs)
         sum = 0.0
         count = 0
-        for p1 in self.modules.wav2vec2.parameters():
+        for p1 in self.modules.wav2vec2.model.encoder.parameters():
             sum += torch.sum(p1.data)
             count += 1
         print(sum)
@@ -71,7 +71,7 @@ class ASR(sb.Brain):
 
         count = 0
         sum = 0.0
-        for p1 in self.modules.wav2vec2_fairseq.parameters():
+        for p1 in self.modules.wav2vec2_fairseq.model.encoder.parameters():
             sum += torch.sum(p1.data)
             count += 1
         print(sum)
