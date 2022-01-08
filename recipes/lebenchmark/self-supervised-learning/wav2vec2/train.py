c4ac98c0098cf3c3b5ee8f80d729b65d78f6e141
Titouan <parcollet.titouan@gmail.com>
2022-01-08 21:36:02 +0100

    lez go

--- recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
+++ recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
@@ -227,10 +227,6 @@ def dataio_prepare(hparams):
         info = torchaudio.info(wav)
         start_seg = int(float(start) * hparams["sample_rate"])
         stop_seg = int(float(end) * hparams["sample_rate"])
-        print(wav)
-        print(start)
-        print(end)
-        print(duration)
         speech_segment = {"file": wav, "start": start_seg, "stop": stop_seg}
         sig = sb.dataio.dataio.read_audio(speech_segment)
         if info.num_channels > 1:
