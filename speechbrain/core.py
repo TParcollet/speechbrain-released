17d57f746f1ba57d012aab05005f34c68bd09795
Titouan <parcollet.titouan@gmail.com>
2022-01-13 16:00:01 +0100

    detach

--- speechbrain/core.py
+++ speechbrain/core.py
@@ -928,7 +928,7 @@ class Brain:
 
         out = self.compute_forward(batch, stage=stage)
         loss = self.compute_objectives(out, batch, stage=stage)
-        return loss
+        return loss.detach().cpu()
 
     def fit(
         self,
