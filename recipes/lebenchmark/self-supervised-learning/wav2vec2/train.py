37590da816e0ebe060ec5d15eb5c56542659a309
Titouan <parcollet.titouan@gmail.com>
2022-01-13 15:31:27 +0100

    cleaning

--- recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
+++ recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
@@ -96,7 +96,7 @@ class W2VBrain(sb.core.Brain):
                     train_stats,
                 ],
             )
-        print(loss)
+
         return loss
 
     def fit_batch(self, batch):
@@ -251,9 +251,6 @@ def dataio_prepare(hparams):
     @sb.utils.data_pipeline.provides("sig")
     def audio_pipeline(wav, start, end, duration):
         info = torchaudio.info(wav)
-        print(wav)
-        print(start)
-        print(end)
         start_seg = int(float(start) * hparams["sample_rate"])
         stop_seg = int(float(end) * hparams["sample_rate"])
         speech_segment = {"file": wav, "start": start_seg, "stop": stop_seg}
