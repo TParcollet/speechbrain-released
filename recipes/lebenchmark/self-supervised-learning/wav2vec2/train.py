8ab839583072ec25766225fa205479038793fce9
Titouan <parcollet.titouan@gmail.com>
2022-01-14 15:40:51 +0100

    remove divide

--- recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
+++ recipes/lebenchmark/self-supervised-learning/wav2vec2/train.py
@@ -168,7 +168,7 @@ class W2VBrain(sb.core.Brain):
         """
 
         out = self.compute_forward(batch, stage=stage)
-        loss = self.compute_objectives(out, batch, stage=stage) / 100
+        loss = self.compute_objectives(out, batch, stage=stage)
         self.check_gradients(loss)
         return loss.detach()
 
