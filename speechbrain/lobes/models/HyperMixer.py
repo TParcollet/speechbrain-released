03edaab69fab9125762dbb61167cdd9992d10b2d
Titouan <parcollet.titouan@gmail.com>
2022-05-12 14:22:25 +0200

    yes

--- speechbrain/lobes/models/HyperMixer.py
+++ speechbrain/lobes/models/HyperMixer.py
@@ -190,7 +190,7 @@ class HyperMixer(nn.Module):
 
     def forward(self, x, wav_len, pad_idx=0, return_weights=False):
 
-        return self.model(x, wav_len, pad_idx=pad_idx)
+        return self.model(x, wav_len, pad_idx, return_weights)
 
 
 class HyperMixerLayer(nn.Module):
