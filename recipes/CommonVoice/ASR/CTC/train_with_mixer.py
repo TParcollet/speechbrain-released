abcfc530a840c08e3d24b33a3c2146d3ab5739b9
Titouan <parcollet.titouan@gmail.com>
2022-05-12 14:27:40 +0200

    shape

--- recipes/CommonVoice/ASR/CTC/train_with_mixer.py
+++ recipes/CommonVoice/ASR/CTC/train_with_mixer.py
@@ -58,7 +58,7 @@ class ASR(sb.core.Brain):
                     pad_idx=0,
                     return_weights=self.hparams.return_weights,
                 )
-                print(W2.shape)
+                print(W2[0].shape)
             else:
                 x = self.modules.mixer(
                     x,
