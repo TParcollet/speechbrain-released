ee499d1ed76d26283da3529e7d6fe290e801abdc
Titouan <parcollet.titouan@gmail.com>
2022-05-13 19:25:01 +0200

    no print

--- speechbrain/nnet/CNN.py
+++ speechbrain/nnet/CNN.py
@@ -598,9 +598,7 @@ class Conv2d(nn.Module):
                 "Padding must be 'same' or 'valid'. Got " + self.padding
             )
 
-        print(x.shape)
         wx = self.conv(x)
-        print(wx.shape)
         if self.unsqueeze:
             wx = wx.squeeze(1)
         wx = wx.transpose(1, -1)
