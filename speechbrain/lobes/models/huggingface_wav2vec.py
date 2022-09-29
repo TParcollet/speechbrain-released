45422bf949b7a053293b807f64008f6bde361268
Titouan <parcollet.titouan@gmail.com>
2022-09-29 11:55:53 +0200

    back to normal

--- speechbrain/lobes/models/huggingface_wav2vec.py
+++ speechbrain/lobes/models/huggingface_wav2vec.py
@@ -23,21 +23,12 @@ from speechbrain.pretrained.fetching import fetch
 # We check if transformers is installed.
 try:
     import transformers
-    from transformers import HubertModel, Wav2Vec2Model
+    from transformers import Wav2Vec2Model, HubertModel
     from transformers import Wav2Vec2Config, HubertConfig
-    from transformers import (
-        Wav2Vec2FeatureExtractor,
-        Wav2Vec2PreTrainedModel,
-    )
+    from transformers import Wav2Vec2FeatureExtractor
+    from transformers import Wav2Vec2ForPreTraining
     from transformers.models.wav2vec2.modeling_wav2vec2 import (
         _compute_mask_indices,
-        Wav2Vec2GumbelVectorQuantizer,
-        Wav2Vec2ForPreTrainingOutput,
-        # Wav2Vec2FeatureProjection,
-        # Wav2Vec2EncoderStableLayerNorm,
-        # Wav2Vec2Encoder,
-        # Wav2Vec2BaseModelOutput,
-        # Wav2Vec2Adapter,
     )
 
 except ImportError:
@@ -46,6 +37,10 @@ except ImportError:
     raise ImportError(MSG)
 
 logger = logging.getLogger(__name__)
+
+HF_models = {"wav2vec2": Wav2Vec2Model, "hubert": HubertModel}
+
+HF_config = {"wav2vec2": Wav2Vec2Config, "hubert": HubertConfig}
 
 
 class HuggingFaceWav2Vec2(nn.Module):
@@ -393,233 +388,3 @@ class HuggingFaceWav2Vec2Pretrain(nn.Module):
             ),
             torch_mask_time_indices,
         )
-
-
-class Wav2Vec2ForPreTraining(Wav2Vec2PreTrainedModel):
-    def __init__(self, config: Wav2Vec2Config):
-        super().__init__(config)
-        self.wav2vec2 = Wav2Vec2Model(config)
-        self.dropout_features = nn.Dropout(config.feat_quantizer_dropout)
-
-        self.quantizer = Wav2Vec2GumbelVectorQuantizer(config)
-
-        # Initialize weights and apply final processing
-        self.post_init()
-
-        # make sure that project_hid & project_q are initialized like normal linear layers
-        self.project_hid = nn.Linear(
-            config.hidden_size, config.proj_codevector_dim
-        )
-        self.project_q = nn.Linear(
-            config.codevector_dim, config.proj_codevector_dim
-        )
-
-    def set_gumbel_temperature(self, temperature: int):
-        """
-        Set the Gumbel softmax temperature to a given value. Only necessary for training
-        """
-        self.quantizer.temperature = temperature
-
-    def freeze_feature_extractor(self):
-        """
-        Calling this function will disable the gradient computation for the feature extractor so that its parameters
-        will not be updated during training.
-        """
-        self.wav2vec2.feature_extractor._freeze_parameters()
-
-    @staticmethod
-    def compute_contrastive_logits(
-        target_features,
-        negative_features,
-        predicted_features,
-        temperature: int = 0.1,
-    ):
-        """
-        Compute logits for contrastive loss based using cosine similarity as the distance measure between
-        `[positive_feature, negative_features]` and `[predicted_features]`. Additionally, temperature can be applied.
-        """
-        target_features = torch.cat([target_features, negative_features], dim=0)
-
-        logits = torch.cosine_similarity(
-            predicted_features.float(), target_features.float(), dim=-1
-        ).type_as(target_features)
-
-        # apply temperature
-        logits = logits / temperature
-        return logits
-
-    def forward(
-        self,
-        input_values,
-        attention_mask=None,
-        mask_time_indices=None,
-        sampled_negative_indices=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        r"""
-        mask_time_indices (`torch.BoolTensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Indices to mask extracted features for contrastive loss. When in training mode, model learns to predict
-            masked extracted features in *config.proj_codevector_dim* space.
-        sampled_negative_indices (`torch.BoolTensor` of shape `(batch_size, sequence_length, num_negatives)`, *optional*):
-            Indices indicating which quantized target vectors are used as negative sampled vectors in contrastive loss.
-            Required input for pre-training.
-        Returns:
-        Example:
-        ```python
-        >>> import torch
-        >>> from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForPreTraining
-        >>> from transformers.models.wav2vec2.modeling_wav2vec2 import _compute_mask_indices
-        >>> from datasets import load_dataset
-        >>> import soundfile as sf
-        >>> feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained("patrickvonplaten/wav2vec2-base")
-        >>> model = Wav2Vec2ForPreTraining.from_pretrained("patrickvonplaten/wav2vec2-base")
-        >>> def map_to_array(batch):
-        ...     speech, _ = sf.read(batch["file"])
-        ...     batch["speech"] = speech
-        ...     return batch
-        >>> ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-        >>> ds = ds.map(map_to_array)
-        >>> input_values = feature_extractor(ds["speech"][0], return_tensors="pt").input_values  # Batch size 1
-        >>> # compute masked indices
-        >>> batch_size, raw_sequence_length = input_values.shape
-        >>> sequence_length = model._get_feat_extract_output_lengths(raw_sequence_length)
-        >>> mask_time_indices = _compute_mask_indices((batch_size, sequence_length), mask_prob=0.2, mask_length=2)
-        >>> with torch.no_grad():
-        ...     outputs = model(input_values, mask_time_indices=mask_time_indices)
-        >>> # compute cosine similarity between predicted (=projected_states) and target (=projected_quantized_states)
-        >>> cosine_sim = torch.cosine_similarity(
-        ...     outputs.projected_states, outputs.projected_quantized_states, dim=-1
-        ... )
-        >>> # show that cosine similarity is much higher than random
-        >>> assert cosine_sim[mask_time_indices].mean() > 0.5
-        >>> # for contrastive loss training model should be put into train mode
-        >>> model.train()
-        >>> loss = model(input_values, mask_time_indices=mask_time_indices).loss
-        ```"""
-
-        return_dict = (
-            return_dict
-            if return_dict is not None
-            else self.config.use_return_dict
-        )
-
-        if mask_time_indices is not None:
-            mask_time_indices = mask_time_indices.to(torch.bool)
-
-        outputs = self.wav2vec2(
-            input_values,
-            attention_mask=attention_mask,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            mask_time_indices=mask_time_indices,
-            return_dict=return_dict,
-        )
-
-        # 1. project all transformed features (including masked) to final vq dim
-        transformer_features = self.project_hid(outputs[0])
-
-        # 2. quantize all (unmasked) extracted features and project to final vq dim
-        extract_features = self.dropout_features(outputs[1])
-
-        if attention_mask is not None:
-            # compute reduced attention_mask correponding to feature vectors
-            attention_mask = self._get_feature_vector_attention_mask(
-                extract_features.shape[1], attention_mask, add_adapter=False
-            )
-
-        quantized_features, codevector_perplexity = self.quantizer(
-            extract_features, mask_time_indices=mask_time_indices
-        )
-        quantized_features = self.project_q(quantized_features)
-
-        loss = contrastive_loss = diversity_loss = None
-        if sampled_negative_indices is not None:
-            batch_size, sequence_length, hidden_size = quantized_features.shape
-
-            # for training, we sample negatives
-            # 3. sample K negatives (distractors) quantized states for contrastive loss
-            # if attention_mask is passed, make sure that padded feature vectors cannot be sampled
-            # sample negative quantized vectors BTC => (BxT)C
-            negative_quantized_features = quantized_features.view(
-                -1, hidden_size
-            )[sampled_negative_indices.long().view(-1)]
-            negative_quantized_features = negative_quantized_features.view(
-                batch_size, sequence_length, -1, hidden_size
-            ).permute(2, 0, 1, 3)
-
-            # 4. compute logits, corresponding to `logs = sim(c_t, [q_t, \sim{q}_t]) / \kappa`
-            # of equation (3) in https://arxiv.org/pdf/2006.11477.pdf
-            logits = self.compute_contrastive_logits(
-                quantized_features[None, :],
-                negative_quantized_features,
-                transformer_features,
-                self.config.contrastive_logits_temperature,
-            )
-
-            # 5. if a negative vector is identical to the positive (i.e. when codebook utilization is low),
-            # its cosine similarity will be masked
-            neg_is_pos = (
-                quantized_features == negative_quantized_features
-            ).all(-1)
-
-            if neg_is_pos.any():
-                logits[1:][neg_is_pos] = float("-inf")
-
-            # 6. compute contrastive loss \mathbf{L}_m = cross_entropy(logs) =
-            # -log(exp(sim(c_t, q_t)/\kappa) / \sum_{\sim{q}} exp(sim(c_t, \sim{q})/\kappa))
-            logits = logits.transpose(0, 2).reshape(-1, logits.size(0))
-            target = (
-                ((1 - mask_time_indices.long()) * -100)
-                .transpose(0, 1)
-                .flatten()
-            )
-
-            contrastive_loss = nn.functional.cross_entropy(
-                logits.float(), target, reduction="sum"
-            )
-            # 7. compute diversity loss: \mathbf{L}_d
-            num_codevectors = (
-                self.config.num_codevectors_per_group
-                * self.config.num_codevector_groups
-            )
-            diversity_loss = (
-                (num_codevectors - codevector_perplexity) / num_codevectors
-            ) * mask_time_indices.sum()
-
-            # 8. \mathbf{L} = \mathbf{L}_m + \alpha * \mathbf{L}_d
-            loss = (
-                contrastive_loss
-                + self.config.diversity_loss_weight * diversity_loss
-            )
-
-        if not return_dict:
-            if loss is not None:
-                return (
-                    loss,
-                    transformer_features,
-                    quantized_features,
-                    codevector_perplexity,
-                ) + outputs[2:]
-            return (
-                transformer_features,
-                quantized_features,
-                codevector_perplexity,
-            ) + outputs[2:]
-
-        return Wav2Vec2ForPreTrainingOutput(
-            loss=loss,
-            projected_states=transformer_features,
-            projected_quantized_states=quantized_features,
-            codevector_perplexity=codevector_perplexity,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-            contrastive_loss=contrastive_loss,
-            diversity_loss=diversity_loss,
-        )
-
-
-HF_models = {"wav2vec2": Wav2Vec2Model, "hubert": HubertModel}
-
-HF_config = {"wav2vec2": Wav2Vec2Config, "hubert": HubertConfig}
